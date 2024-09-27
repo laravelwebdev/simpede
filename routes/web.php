bedd3c1ae8bef2d40c2c11d3b07f2392360e8bb4
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-09-28 07:37:56 +0800

    revert

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Nova\Http\Middleware\Authenticate;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/changerole/{role}', 'App\Http\Controllers\RoleController@changeRole')
     ->name('changerole')
     ->middleware(Authenticate::class);

2ca7a2cd3539c561294afd059cd22ecaf20fd52b
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-10-24 13:50:34 +0000

    Refactor templates.sql to add new template files

--- database/dump_sql/templates.sql
+++ database/dump_sql/templates.sql
@@ -8,10 +8,13 @@ SET time_zone = "+00:00";
 
 
 INSERT INTO `templates` (`id`, `nama`, `jenis`, `file`, `created_at`, `updated_at`) VALUES
-(1, 'Template Import Kode Arsip','import', 'HMrafy1OQUdCv20aF7zFgb12BU7mkpCMk5yH4IUE.xlsx', '2024-08-16 11:04:17', '2024-08-16 11:04:17'),
+(1, 'Template Import Kode Arsip', 'import', 'HMrafy1OQUdCv20aF7zFgb12BU7mkpCMk5yH4IUE.xlsx', '2024-08-16 11:04:17', '2024-08-16 11:04:17'),
 (2, 'Template Import Mitra', 'import', 'vs4vjnKmcr8CPUrONoj44wQbI1ntrEeSwLJvsDRD.xlsx', '2024-08-16 15:32:32', '2024-08-16 15:34:15'),
 (3, 'Template Kerangka Acuan Kerja', 'kak', 'ReAdPXzRYWqgpho3W0mX4U3rxg3UfZ3F4MmKlxsP.docx', '2024-09-02 15:05:11', '2024-09-05 09:13:09'),
-(4, 'Template SPJ','spj', 'd2z8X186YFymCM29dPa84LH7rTrqljrmfhmyc7C4.docx', '2024-09-08 08:13:31', '2024-09-08 09:41:00');
+(4, 'Template SPJ', 'spj', 'd2z8X186YFymCM29dPa84LH7rTrqljrmfhmyc7C4.docx', '2024-09-08 08:13:31', '2024-09-08 09:41:00'),
+(5, 'Template SK Petugas', 'sk', 'JmHXF4rvAMCnZQg6zx8jjfvZAygCsh0hpZcRcSJd.docx', '2024-10-19 08:47:00', '2024-10-19 08:47:00'),
+(6, 'Template Surat Tugas', 'st', '6ALNH1DPbuAbXP3t3muIDuCOQ7Wjqd1WTR9zqXlS.docx', '2024-10-19 08:47:26', '2024-10-19 08:47:26'),
+(7, 'Template Surat Tugas dengan Lampiran', 'st', 'uYxj7Ir0cAiOBnrQ9EPEi5fqHuStnbVSHOhFijjl.docx', '2024-10-19 08:47:48', '2024-10-19 08:47:48');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

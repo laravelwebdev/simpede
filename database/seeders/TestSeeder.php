c727c2b0a10337eb3485a560e331d279b786be41
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2024-10-25 22:48:47 +0000

    Refactor database seeders to add new SQL dump files

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -22,7 +22,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class DatabaseSeeder extends Seeder
+class TestSeeder extends Seeder
 {
     /**
      * Seed the application's database.
@@ -31,37 +31,7 @@ class DatabaseSeeder extends Seeder
     {
         // Eloquent::unguard();
         DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/users.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/data_pegawais.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/pengelolas.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/tata_naskahs.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/kode_naskahs.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/jenis_naskahs.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/kode_arsips.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/templates.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/unit_kerjas.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/derajat_naskahs.sql')
-        );
-        DB::unprepared(
-            file_get_contents(database_path().'/dump_sql/naskah_defaults.sql')
+            file_get_contents(database_path().'/dump_sql/dummy.sql')
         );
         DataPegawai::cache()->updateAll();
         DerajatNaskah::cache()->updateAll();

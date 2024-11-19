312a1f797389ea74c42d7164fbe9e74bfad331fd
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2024-11-19 04:50:57 +0000

    Add notification for Pemeliharaan creation; prompt user to complete maintenance details

--- app/Models/Pemeliharaan.php
+++ app/Models/Pemeliharaan.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Nova\Notifications\NovaNotification;
+use Laravel\Nova\Nova;
+use Laravel\Nova\URL;
 
 class Pemeliharaan extends Model
 {
@@ -18,6 +22,13 @@ class Pemeliharaan extends Model
     {
         static::creating(function (Pemeliharaan $pemeliharaan) {
             $pemeliharaan->status = 'dibuat';
+            User::find(Auth::user()->id)->notify(
+                NovaNotification::make()
+                    ->message('Anda mengajukan anggaran pemeliharaan. Mohon lengkapi detail pemeliharaan yang dilakukan!')
+                    ->action('Lihat', URL::remote(Nova::path().'/resources/'.\App\Nova\Pemeliharaan::uriKey().'/'.$pemeliharaan->id))
+                    ->icon('exclamation')
+                    ->type('error')
+            );
         });
         static::deleting(function (Pemeliharaan $pemeliharaan) {
             //TODO: Hapus barang pemeliharaan

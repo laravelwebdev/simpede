bad19b5fd4d95eb1bb8ab85b16c9b0b4cacfb19e
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-11-20 09:26:35 +0000

    Refactor DaftarPemeliharaanPolicy viewAny method to simplify access control logic

--- app/Policies/DaftarPemeliharaanPolicy.php
+++ app/Policies/DaftarPemeliharaanPolicy.php
@@ -13,15 +13,9 @@ class DaftarPemeliharaanPolicy
      */
     public function viewAny(): bool
     {
-        return Nova::whenServing(function (NovaRequest $request) {
-            if ($request->viaResource == 'pemeliharaans' || str_contains(request()->url(), '/pemeliharaans')) {
-                return Policy::make()
-                    ->allowedFor('kasubbag,koordinator,anggota,bmn')
-                    ->get();
-            }
-
-            return false;
-        });
+        return Policy::make()
+            ->allowedFor('kasubbag,koordinator,anggota,bmn')
+            ->get();
 
     }
 

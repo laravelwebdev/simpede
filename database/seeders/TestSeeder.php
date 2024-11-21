f7edab3fcb02f28621b6e984d43e8c9528d2dd6d
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-11-21 08:54:18 +0000

    Update TestSeeder to include additional model cache updates

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -6,15 +6,20 @@ use App\Models\DataPegawai;
 use App\Models\DerajatNaskah;
 use App\Models\Dipa;
 use App\Models\HargaSatuan;
+use App\Models\JenisBelanja;
 use App\Models\JenisKontrak;
 use App\Models\JenisNaskah;
 use App\Models\KamusAnggaran;
+use App\Models\KepkaMitra;
 use App\Models\KodeArsip;
 use App\Models\KodeNaskah;
+use App\Models\MasterBarangPemeliharaan;
+use App\Models\MasterPersediaan;
 use App\Models\MataAnggaran;
 use App\Models\Mitra;
 use App\Models\NaskahDefault;
 use App\Models\Pengelola;
+use App\Models\TargetSerapanAnggaran;
 use App\Models\TataNaskah;
 use App\Models\Template;
 use App\Models\UnitKerja;
@@ -37,18 +42,27 @@ class TestSeeder extends Seeder
         DerajatNaskah::cache()->updateAll();
         Dipa::cache()->updateAll();
         HargaSatuan::cache()->updateAll();
+        JenisBelanja::cache()->updateAll();
         JenisKontrak::cache()->updateAll();
         JenisNaskah::cache()->updateAll();
         KamusAnggaran::cache()->updateAll();
+        KepkaMitra::cache()->updateAll();
         KodeArsip::cache()->updateAll();
         KodeNaskah::cache()->updateAll();
+        MasterBarangPemeliharaan::cache()->updateAll();
+        MasterPersediaan::cache()->updateAll();
         MataAnggaran::cache()->updateAll();
         Mitra::cache()->updateAll();
+        NaskahDefault::cache()->updateAll();
         Pengelola::cache()->updateAll();
+        TargetSerapanAnggaran::cache()->updateAll();
         TataNaskah::cache()->updateAll();
         Template::cache()->updateAll();
         UnitKerja::cache()->updateAll();
         User::cache()->updateAll();
-        NaskahDefault::cache()->updateAll();
+
+
+
+
     }
 }

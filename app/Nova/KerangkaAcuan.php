2fa82d34e218ccb8b3eb613c068cf40aaa6190ce
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-06 06:26:34 +0800

    Restrict access in KerangkaAcuan indexQuery to exclude 'arsiparis' role

--- app/Nova/KerangkaAcuan.php
+++ app/Nova/KerangkaAcuan.php
@@ -39,7 +39,7 @@ class KerangkaAcuan extends Resource
     public static function indexQuery(NovaRequest $request, $query)
     {
         $query->whereYear('tanggal', session('year'));
-        if (Policy::make()->allowedFor('koordinator,anggota')->get()) {
+        if (Policy::make()->allowedFor('koordinator,anggota')->notAllowedFor('arsiparis')->get()) {
             return $query->where('unit_kerja_id', Helper::getDataPegawaiByUserId($request->user()->id, now())->unit_kerja_id);
         }
 

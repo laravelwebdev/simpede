2065f7fa2eecbeabc672da42dc20616dfe7f3064
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-08 07:22:24 +0800

    Add fillable properties to KakSp2d model for mass assignment

--- app/Models/KakSp2d.php
+++ app/Models/KakSp2d.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class KakSp2d extends Model
 {
+    protected $fillable = [
+        'kerangka_acuan_id',
+        'daftar_sp2d_id',
+    ];
     protected $table = 'kak_sp2d';
 }

b92c02ded33a21601a3ce6b5d3e4ba5b39d74fd7
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-08 12:10:36 +0800

    Remove global scope and filterable field for 'daftarSp2d' in KerangkaAcuan: Simplify model and UI components

--- app/Models/KerangkaAcuan.php
+++ app/Models/KerangkaAcuan.php
@@ -53,9 +53,9 @@ class KerangkaAcuan extends Model
 
     protected static function booted(): void
     {
-        static::addGlobalScope('with-daftar-sp2d-count', function (Builder $builder) {
-            $builder->withCount('daftarSp2d');
-        });
+        // static::addGlobalScope('with-daftar-sp2d-count', function (Builder $builder) {
+        //     $builder->withCount('daftarSp2d');
+        // });
 
         static::creating(function (KerangkaAcuan $kak) {
             $kak->status = 'dibuat';

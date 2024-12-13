223a179277e63130d61a21086e85d5605166153f
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-14 06:26:19 +0800

    Update KakSp2d: Set default status on saving and deleting events; rename SP2D field to 'Jumlah SP2D'

--- app/Models/KakSp2d.php
+++ app/Models/KakSp2d.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class KakSp2d extends Pivot
 {
     protected $table = 'kak_sp2d';
+    
+    protected static function booted(): void
+    {
+        static::saving(function (KakSp2d $kakSp2d) {
+            $kakSp2d->status = 'dibuat';
+        });
+        static::deleting(function (KakSp2d $kakSp2d) {
+            $kakSp2d->status = 'dihapus';
+        });
+    }
 }

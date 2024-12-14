1c22788f7f58aa795b7029cbc50ca1072f7856f6
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-14 10:18:41 +0800

    Update KakSp2d: Enhance file operations to include SP2D PDF handling during save and delete events

--- app/Models/KakSp2d.php
+++ app/Models/KakSp2d.php
@@ -8,17 +8,22 @@ use Illuminate\Support\Facades\Storage;
 class KakSp2d extends Pivot
 {
     protected $table = 'kak_sp2d';
-    
+
     protected static function booted(): void
     {
         static::saving(function (KakSp2d $kakSp2d) {
-           Storage::disk('arsip')
-           ->copy(DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->arsip_spm, 
-           session('year').'/'.'arsip-dokumens'.'/'.$kakSp2d->kerangka_acuan_id . '/SPM_' .DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
+            Storage::disk('arsip')
+                ->copy(DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->arsip_spm,
+                    session('year').'/'.'arsip-dokumens'.'/'.$kakSp2d->kerangka_acuan_id.'/SPM_'.DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
+            Storage::disk('arsip')
+                ->copy(DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->arsip_sp2d,
+                    session('year').'/'.'arsip-dokumens'.'/'.$kakSp2d->kerangka_acuan_id.'/SP2D_'.DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
         });
         static::deleting(function (KakSp2d $kakSp2d) {
             Storage::disk('arsip')
-            ->delete(session('year').'/'.'arsip-dokumens'.'/'.$kakSp2d->kerangka_acuan_id . '/SPM_' .DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
+                ->delete(session('year').'/'.'arsip-dokumens'.'/'.$kakSp2d->kerangka_acuan_id.'/SPM_'.DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
+            Storage::disk('arsip')
+                ->delete(session('year').'/'.'arsip-dokumens'.'/'.$kakSp2d->kerangka_acuan_id.'/SP2D_'.DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
         });
     }
 }

e8aeb4e046e09ad33f138c44ccfb87aa820b68bf
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-14 18:05:51 +0800

    Refactor ArsipController show method to handle year parameter more gracefully

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -6,14 +6,13 @@ use Illuminate\Http\Request;
 
 class ArsipController extends Controller
 {
-    public function show(string $tahun = Date('Y'))
+    public function show(string $tahun)
     {
         $tahun = (int) $tahun;
-        $tahun = $tahun < 2000 ? Date('Y') : $tahun;
+        $tahun = $tahun ?? Date('Y') ;
 
         return view('arsip-dokumen', [
             'tahun' => $tahun,
-            'files' => $this->getFiles($tahun),
         ]);
     }
 }

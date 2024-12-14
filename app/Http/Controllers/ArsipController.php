fd3210c320b4cc2356dc134e6c7a9780ef86bfc8
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-14 19:02:09 +0800

    Refactor ArsipController show method to streamline year handling and remove unnecessary error check

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Dipa;
-use App\Models\MataAnggaran;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ArsipController extends Controller
@@ -14,13 +12,9 @@ class ArsipController extends Controller
         $tahun = (int) $tahun;
         $tahun = $tahun == 0 ? date('Y') : $tahun;
         $dipa = Dipa::where('tahun', $tahun)->first();
-        
-        if (!$dipa) {
-            return redirect()->back()->with('error', 'Data DIPA tidak ditemukan untuk tahun tersebut.');
-        }
 
         $data = DB::table('mata_anggarans')
-            ->selectRaw('DISTINCT MID(mak,11,8) as KRO')    
+            ->selectRaw('DISTINCT MID(mak,11,8) as KRO')
             ->where('dipa_id', $dipa->id)->get();
 
         return view('arsip-dokumen', [

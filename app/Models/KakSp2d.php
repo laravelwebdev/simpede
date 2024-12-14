e63ac499b2edf6070563fcfa0a4957a4a915e4fd
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-14 10:04:59 +0800

    Update KakSp2d: Append '.pdf' extension to the destination file name during copy operation

--- app/Models/KakSp2d.php
+++ app/Models/KakSp2d.php
@@ -14,7 +14,7 @@ class KakSp2d extends Pivot
         static::saving(function (KakSp2d $kakSp2d) {
            Storage::disk('arsip')
            ->copy(DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->arsip_spm, 
-           DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp);
+           DaftarSp2d::find($kakSp2d->daftar_sp2d_id)->nomor_spp.'.pdf');
         });
         static::deleting(function (KakSp2d $kakSp2d) {
             $kakSp2d->status = 'dihapus';

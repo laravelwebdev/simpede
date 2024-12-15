34d1c565ad887b4054a39a7796e815efd0ed3676
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-15 14:43:05 +0800

    Add ShareLink model, policy, Nova resource, and migration for share links functionality

---
+++ app/Nova/ShareLink.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Nova;
+
+use App\Helpers\Helper;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class ShareLink extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\ShareLink>
+     */
+    public static $model = \App\Models\ShareLink::class;
+
+    public static function label()
+    {
+        return 'Share Link SPJ';
+    }
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'tahun';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'tahun',
+    ];
+
+    public static $globallySearchable = false;
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            Select::make('Tahun')
+                ->sortable()
+                ->searchable()
+                ->rules('required')
+                ->options(Helper::setOptionTahunDipa()),
+            Text::make('Link')
+                ->exceptOnForms()
+                ->copyable(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
---
+++ app/Policies/ShareLinkPolicy.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Policies;
+
+use App\Helpers\Policy;
+
+class ShareLinkPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(): bool
+    {
+        return Policy::make()
+            ->allowedFor('admin,koordinator,ppk,anggota,arsiparis')
+            ->get();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(): bool
+    {
+        return Policy::make()
+            ->allowedFor('admin,koordinator,ppk,anggota,arsiparis')
+            ->get();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(): bool
+    {
+        return Policy::make()
+            ->allowedFor('admin')
+            ->get();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(): bool
+    {
+        return Policy::make()
+            ->allowedFor('admin')
+            ->get();
+    }
+
+    /**
+     * Determine whether the user can replicate the model.
+     */
+    public function replicate(): bool
+    {
+        return false;
+    }
+}

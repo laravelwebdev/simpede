8cb3b0666abb99d195dc151c4079a90185c327a8
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-18 19:17:26 +0800

    Refactor query method in RekapHonorMitra lens to use HonorKegiatan model for improved clarity and maintainability

--- app/Nova/Lenses/RekapHonorMitra.php
+++ app/Nova/Lenses/RekapHonorMitra.php
@@ -52,7 +52,7 @@ class RekapHonorMitra extends Lens
                     'valid_sbml' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan) < sbml'),
                 ])
                 ->whereIn('honor_kegiatan_id', function ($query) use ($request, $filtered_bulan) {
-                    $request->withFilters($query->select('id')->from('honor_kegiatans')
+                    $request->withFilters(\App\Models\HonorKegiatan::query()
                         ->where('tahun', session('year'))
                         ->when(! empty($filtered_bulan), function ($query) use ($filtered_bulan) {
                             return $query->where('bulan', $filtered_bulan);

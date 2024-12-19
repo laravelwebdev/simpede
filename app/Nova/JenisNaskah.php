2dfde76b755ae949dcf18030c9af7b093b4a376d
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2024-12-19 23:52:38 +0000

    Enable credits disabling for Filepond template field

--- app/Nova/JenisNaskah.php
+++ app/Nova/JenisNaskah.php
@@ -60,6 +60,7 @@ class JenisNaskah extends Resource
                 ->rules('required'),
             Filepond::make('Template')
                 ->disk('template_naskah')
+                ->disableCredits()
                 ->mimesTypes(['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'])
                 ->onlyOnForms()
                 ->prunable(),

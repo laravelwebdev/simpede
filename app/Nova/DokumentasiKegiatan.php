698f54ce98570e01aa59525d7dcf531e2d29b739
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-22 07:05:29 +0800

    Disable download option for 'Foto' field in DokumentasiKegiatan Nova resource

--- app/Nova/DokumentasiKegiatan.php
+++ app/Nova/DokumentasiKegiatan.php
@@ -65,7 +65,7 @@ class DokumentasiKegiatan extends Resource
             foreach ($this->file as $file) {
             $imageFields[] = Image::make('Foto', fn () => $file)
                 ->disk('dokumentasi')
-                ->download($file)
+                ->disableDownload()
                 ->onlyOnDetail();
             }
         }

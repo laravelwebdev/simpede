0c8d22c67582eecec299da9769786b98f842c889
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2024-12-23 02:55:51 +0000

    Add reset saldo feature to ImportMasterPersediaan action

--- app/Nova/Actions/ImportMasterPersediaan.php
+++ app/Nova/Actions/ImportMasterPersediaan.php
@@ -3,13 +3,16 @@
 namespace App\Nova\Actions;
 
 use App\Helpers\Helper;
+use App\Models\BarangPersediaan;
 use App\Models\MasterPersediaan;
+use App\Models\PersediaanMasuk;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Nova\Actions\Action;
 use Laravel\Nova\Fields\ActionFields;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\File;
 use Laravel\Nova\Fields\Heading;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -28,9 +31,18 @@ class ImportMasterPersediaan extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
+        $id_persediaan_masuk = null;
+        if ($fields->reset_saldo) {
+            $persediaan_masuk = new PersediaanMasuk;
+            $persediaan_masuk->tanggal_dokumen = session('year') - 1 .'-12-31';
+            $persediaan_masuk->tanggal_buku = session('year') - 1 .'-12-31';
+            $persediaan_masuk->rincian = 'Saldo Awal';
+            $persediaan_masuk->save();
+            $id_persediaan_masuk = $persediaan_masuk->id;
+        }
         MasterPersediaan::cache()->disable();
         MasterPersediaan::query()->update(['updated_at' => null]);
-        (new FastExcel)->import($fields->file, function ($row) {
+        (new FastExcel)->import($fields->file, function ($row) use ($fields, $id_persediaan_masuk) {
             $masterPersediaan = MasterPersediaan::firstOrNew(
                 [
                     'kode' => $row['kode'],
@@ -41,6 +53,16 @@ class ImportMasterPersediaan extends Action
             $masterPersediaan->updated_at = now();
 
             $masterPersediaan->save();
+            $masterPersediaanId = $masterPersediaan->id;
+            if ($fields->reset_sald && $row['saldo'] > 0) {
+                $persediaan = new BarangPersediaan;
+                $persediaan->volume = $row['saldo'];
+                $persediaan->tanggal_transaksi = session('year') - 1 .'-12-31';
+                $persediaan->master_persediaan_id = $masterPersediaanId;
+                $persediaan->barang_persediaanable_id = $id_persediaan_masuk;
+                $persediaan->barang_persediaanable_type = 'App\Models\PersediaanMasuk';
+                $persediaan->save();
+            }
         });
         $ids = MasterPersediaan::where('updated_at', null)->get()->pluck('id');
         MasterPersediaan::destroy($ids);
@@ -61,6 +83,9 @@ class ImportMasterPersediaan extends Action
             File::make('File')
                 ->rules('required', 'mimes:xlsx')
                 ->acceptedTypes('.xlsx')->help('Data akan diperbaharui dengan data baru'),
+            Boolean::make('Reset Saldo Awal?', 'reset-saldo')
+                ->default(false)
+                ->help('Hanya centang jika Anda mengerti apa yang dilakukan. Hanya dipakai pada saat awal implementasi Aplikasi'),
             Heading::make('<a href = "'.Storage::disk('templates')->url(Helper::getTemplatePathByName('Template Import Master Persediaan')['filename']).'">Unduh Template</a>')
                 ->asHtml(),
         ];

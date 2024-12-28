98ce4a72909022ac1671f42dc113574c224a01f0
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-28 09:32:44 +0000

    Refactor WebhookController to check for data existence before processing

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,20 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\DaftarReminder;
-use Illuminate\Support\Facades\Storage;
 
 class WebhookController extends Controller
 {
-
     public function handle()
     {
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
-        $id = $data['id'];
-        $status = $data['status'];    
-    
-        if (isset($id)) {
-            DaftarReminder::where('message_id', $id)->update(['status' => $status]);
+        if (isset($data)) {
+            $id = $data['id'];
+            $status = $data['status'];
+
+            if (isset($id)) {
+                DaftarReminder::where('message_id', $id)->update(['status' => $status]);
+            }
         }
+
     }
 }

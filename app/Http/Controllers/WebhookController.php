1f650397bc71d7ec770501c6e57d1056e1056f17
Muhlis Abdi <enterpriseabdi@gmail.com>
2024-12-28 10:00:57 +0000

    Set JSON content type header in WebhookController

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -8,6 +8,7 @@ class WebhookController extends Controller
 {
     public function handle()
     {
+        header('Content-Type: application/json; charset=utf-8');
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
         if (isset($data)) {

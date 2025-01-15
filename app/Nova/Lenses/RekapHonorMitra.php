f65a758c591d78c24c41290ced59526fc15e6544
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-15 17:19:49 +0800

    Refactor RekapHonorMitra lens query by consolidating select fields for improved clarity and performance

--- app/Nova/Lenses/RekapHonorMitra.php
+++ app/Nova/Lenses/RekapHonorMitra.php
@@ -44,11 +44,7 @@ class RekapHonorMitra extends Lens
 
         return $request->withoutTableOrderPrefix()->withOrdering(
             $query->select('bulan', 'jenis_kontrak_id', 'nama', 'mitra_id')
-                ->addSelect([
-                    // 'jumlah_kegiatan' => fn ($query) => $query->selectRaw('count(DISTINCT honor_kegiatan_id)'),
-                    'nilai_kontrak' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan)'),
-                    // 'valid_sbml' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan) < sbml'),
-                ])
+                ->selectRaw('count(DISTINCT honor_kegiatan_id) as jumlah_kegiatan, sum(volume_realisasi * harga_satuan) as nilai_kontrak, sum(volume_realisasi * harga_satuan) < sbml as valid_sbml')
                 ->whereIn('honor_kegiatan_id', function ($query) use ($request, $filtered_bulan) {
                     $request->withFilters($query->select('id')->from('honor_kegiatans')
                         ->where('tahun', session('year'))

ec8cb591f25e7bb838ad109602a0e2247a9ef523
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-15 17:10:30 +0800

    Refactor RekapHonorMitra lens query by commenting out unused select fields for clarity

--- app/Nova/Lenses/RekapHonorMitra.php
+++ app/Nova/Lenses/RekapHonorMitra.php
@@ -44,11 +44,11 @@ class RekapHonorMitra extends Lens
 
         return $request->withoutTableOrderPrefix()->withOrdering(
             $query->select('bulan', 'jenis_kontrak_id', 'nama', 'mitra_id')
-                ->addSelect([
-                    'jumlah_kegiatan' => fn ($query) => $query->selectRaw('count(DISTINCT honor_kegiatan_id)'),
-                    'nilai_kontrak' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan)'),
-                    'valid_sbml' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan) < sbml'),
-                ])
+                // ->addSelect([
+                //     'jumlah_kegiatan' => fn ($query) => $query->selectRaw('count(DISTINCT honor_kegiatan_id)'),
+                //     'nilai_kontrak' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan)'),
+                //     'valid_sbml' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan) < sbml'),
+                // ])
                 ->whereIn('honor_kegiatan_id', function ($query) use ($request, $filtered_bulan) {
                     $request->withFilters($query->select('id')->from('honor_kegiatans')
                         ->where('tahun', session('year'))

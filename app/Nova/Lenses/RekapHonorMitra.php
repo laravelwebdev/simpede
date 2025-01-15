fa6213bf0413b49c887ad2652330bade4d496971
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-15 17:13:34 +0800

    Refactor RekapHonorMitra lens query by uncommenting 'nilai_kontrak' and 'valid_sbml' select fields for clarity and performance

--- app/Nova/Lenses/RekapHonorMitra.php
+++ app/Nova/Lenses/RekapHonorMitra.php
@@ -44,11 +44,11 @@ class RekapHonorMitra extends Lens
 
         return $request->withoutTableOrderPrefix()->withOrdering(
             $query->select('bulan', 'jenis_kontrak_id', 'nama', 'mitra_id')
-                // ->addSelect([
-                //     'jumlah_kegiatan' => fn ($query) => $query->selectRaw('count(DISTINCT honor_kegiatan_id)'),
-                //     'nilai_kontrak' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan)'),
-                //     'valid_sbml' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan) < sbml'),
-                // ])
+                ->addSelect([
+                    // 'jumlah_kegiatan' => fn ($query) => $query->selectRaw('count(DISTINCT honor_kegiatan_id)'),
+                    'nilai_kontrak' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan)'),
+                    'valid_sbml' => fn ($query) => $query->selectRaw('sum(volume_realisasi * harga_satuan) < sbml'),
+                ])
                 ->whereIn('honor_kegiatan_id', function ($query) use ($request, $filtered_bulan) {
                     $request->withFilters($query->select('id')->from('honor_kegiatans')
                         ->where('tahun', session('year'))
@@ -61,9 +61,10 @@ class RekapHonorMitra extends Lens
                 ->join('daftar_honor_mitras', 'mitras.id', '=', 'daftar_honor_mitras.mitra_id')
                 ->join('honor_kegiatans', 'honor_kegiatans.id', '=', 'daftar_honor_mitras.honor_kegiatan_id')
                 ->join('jenis_kontraks', 'jenis_kontraks.id', '=', 'honor_kegiatans.jenis_kontrak_id')
-                // ->groupBy('bulan', 'mitra_id', 'nama', 'nik', 'sbml', 'jenis_kontrak_id', 'volume_realisasi', 'harga_satuan')
+                ->groupBy('bulan', 'mitra_id', 'nama', 'nik', 'sbml', 'jenis_kontrak_id', 'volume_realisasi', 'harga_satuan')
                 ->orderBy('jenis_kontrak_id', 'asc')
-                ->orderBy('bulan', 'desc'));
+                ->orderBy('bulan', 'desc')
+                ->orderBy('nilai_kontrak', 'desc'));
     }
 
     /**

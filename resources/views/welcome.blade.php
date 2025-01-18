810e19d2d6819dd4ff36e59fe85d83d6ada7dd67
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-01-19 06:56:14 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -325,7 +325,7 @@
             </div>
             <div class="footer">
                 <p>
-                Simpede - v 2.1
+                Simpede - v 2.2.0
         </p>
                 <p>                
                 Copyright &copy; 2021 - 

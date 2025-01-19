1cc1a8b0ffa68bab4e1d72c7c877054d56169a51
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-01-19 23:50:42 +0000

    Fix default date value for 'tanggal' field in Download action

--- app/Nova/Actions/Download.php
+++ app/Nova/Actions/Download.php
@@ -97,7 +97,7 @@ class Download extends Action
         if ($this->withTanggal) {
             $fields[] = Date::make('Tanggal', 'tanggal')
                 ->rules('required', 'date', 'before_or_equal:today')
-                ->default(now());
+                ->default(Date::now());
         }
 
         if ($this->withOptionPengelola) {

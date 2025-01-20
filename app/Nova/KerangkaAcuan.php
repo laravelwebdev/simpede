a7260dbbc942d3cfc64e467f3867408d8424a4f6
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-01-20 01:06:14 +0000

    Add TestAction class and integrate it into KerangkaAcuan actions

--- app/Nova/KerangkaAcuan.php
+++ app/Nova/KerangkaAcuan.php
@@ -7,6 +7,7 @@ use App\Helpers\Policy;
 use App\Models\Dipa;
 use App\Nova\Actions\AddPerjalananDinas;
 use App\Nova\Actions\Download;
+use App\Nova\Actions\TestAction;
 use App\Nova\Filters\StatusFilter;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Nova\Fields\BelongsTo;
@@ -203,6 +204,8 @@ class KerangkaAcuan extends Resource
                })
                ->exceptOnIndex();
         }
+
+        $actions[] = TestAction::make()->showinline();
 
         return $actions;
     }

ed3a701c9ab2fa0d35c5e23ba4851b4efbc4abad
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-01-20 23:59:30 +0000

    Add cache clearing step after updating dependencies in Update command

--- app/Console/Commands/Update.php
+++ app/Console/Commands/Update.php
@@ -49,5 +49,12 @@ class Update extends Command
         }
         $this->info('Dependencies sukses diupdate');
 
+        $process = Process::fromShellCommandline("$composer clear-cache", base_path(), ['COMPOSER_HOME' => '../../.cache/composer']);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            $this->error($process->getErrorOutput());
+            return 1; // Return non-zero exit code to indicate failure
+        }
+        $this->info('Cache composer sukses dihapus');
     }
 }

e1d94a9dfcd5df3f1b42bea5410fce88f6537068
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-20 23:46:28 +0800

    Update Update command to remove --no-dev flag and adjust COMPOSER_HOME path

--- app/Console/Commands/Update.php
+++ app/Console/Commands/Update.php
@@ -37,7 +37,7 @@ class Update extends Command
         // $composer = config('app.composer');
         // $devFlag = $this->option('dev') ? '' : '--no-dev';
         // shell_exec('composer2 update');
-        app()->make(Composer::class)->run(['update --no-dev']);
+        app()->make(Composer::class)->run(['update']);
 
         $this->info($process->getOutput());
     }

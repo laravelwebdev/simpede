8757d999dec4f52a76e5c6bee94c36ace6c99ca0
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-22 20:52:48 +0800

    Refactor MetricTrend calculation to use 'tanggal' alias for improved clarity in monthly data counting

--- app/Nova/Metrics/MetricTrend.php
+++ app/Nova/Metrics/MetricTrend.php
@@ -3,7 +3,6 @@
 namespace App\Nova\Metrics;
 
 use DateTimeInterface;
-use Illuminate\Support\Facades\DB;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Metrics\Trend;
 use Laravel\Nova\Metrics\TrendResult;
@@ -25,7 +24,7 @@ class MetricTrend extends Trend
      */
     public function calculate(NovaRequest $request): TrendResult
     {
-        return $this->countByMonths($request, $this->model, DB::raw('tanggal'));
+        return $this->countByMonths($request, $this->model, 'tanggal as tanggal');
     }
 
     /**

2b143ad23d6beccce46bca2d1b6d06ef89727a1f
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-01-22 23:03:39 +0800

    Fix syntax in MetricKeberadaan calculation for improved readability

--- app/Nova/Metrics/MetricKeberadaan.php
+++ app/Nova/Metrics/MetricKeberadaan.php
@@ -29,7 +29,8 @@ class MetricKeberadaan extends Partition
     {
         $results = $this->model->newQuery()
             ->selectRaw("SUM (CASE WHEN {$this->column} IS NOT NULL THEN 1 ELSE 0 END) as Ada, SUM (CASE WHEN {$this->column} IS NULL THEN 1 ELSE 0 END) as Tidak")
-            ->get()->toArray();
+            ->get()
+            ->toArray();
 
         return $this->result($results)
             ->colors([

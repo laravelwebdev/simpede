38523e2287563c9f578968b80be93371acf7fc0c
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-01-23 08:59:24 +0000

    Fix validation rules in ResponRate Nova resource to ensure proper handling of nullable values

--- app/Nova/ResponRate.php
+++ app/Nova/ResponRate.php
@@ -59,7 +59,8 @@ class ResponRate extends Resource
             Numeric::make('Target Sampel Setahun', 'target')
                 ->rules('required', 'integer', 'gte:0'),
             Numeric::make('Realisasi Triwulan I (Kumulatif)', 'realisasi_tw1')
-                ->rules('lte:target')
+            //BUG: rule bail not working
+                ->rules('nullable', 'bail', 'integer', 'gte:0', 'lte:target')
                 ->help('Jumlah Kumulatif sampel yang berhasil dicacah sampai dengan Triwulan I'),
             Numeric::make('Realisasi Triwulan II (Kumulatif)', 'realisasi_tw2')
                 ->rules('nullable', 'bail', 'integer', 'gte:realisasi_tw1', 'lte:target')

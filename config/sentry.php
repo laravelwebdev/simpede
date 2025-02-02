3ab26cb38f6402aa968a6bd4e2b561384623d1f0
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-02-02 04:23:29 +0000

    Remove commented logger configuration from Sentry settings for clarity

--- config/sentry.php
+++ config/sentry.php
@@ -13,9 +13,6 @@ return [
     // @see https://spotlightjs.com/
     // 'spotlight' => env('SENTRY_SPOTLIGHT', false),
 
-    // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#logger
-    // 'logger' => Sentry\Logger\DebugFileLogger::class, // By default this will log to `storage_path('logs/sentry.log')`
-
     // The release version of your application
     // Example with dynamic git hash: trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD'))
     'release' => env('SENTRY_RELEASE'),

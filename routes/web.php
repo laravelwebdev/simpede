5014aace9ec4d787a039007f29e7d27db90dfde8
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-02-11 05:55:58 +0800

    Refactor routes to group arsip-dokumen endpoints under middleware for improved organization and security

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,20 @@ use Laravel\Nova\Nova;
 
 Route::get('/', HomeController::class)->name('welcome');
 
-Route::get('/dump-download/{filename}', DumpDownloadController::class)
+Route::post('/dump-download/{filename}', DumpDownloadController::class)
     ->name('dump-download')
     ->middleware(Authenticate::class)
     ->prefix(Nova::path());
-Route::get('/arsip-dokumen/{token}', [ArsipController::class, 'perKro'])
-    ->name('arsip-per-kro')
-    ->middleware(ValidateAccessToken::class)
-    ->prefix(Nova::path());
-Route::get('/arsip-dokumen/{token}/kro/{kro}', [ArsipController::class, 'perDetail'])
-    ->name('arsip-per-detail')
-    ->middleware(ValidateAccessToken::class)
-    ->prefix(Nova::path());
-Route::get('/arsip-dokumen/{token}/coa/{coa}', [ArsipController::class, 'perKak'])
-    ->name('arsip-per-kak')
-    ->middleware(ValidateAccessToken::class)
-    ->prefix(Nova::path());
-Route::get('/arsip-dokumen/{token}/kak/{kak}', [ArsipController::class, 'daftarFile'])
-    ->name('daftar-file')
-    ->middleware(ValidateAccessToken::class)
-    ->prefix(Nova::path());
+
+Route::middleware([ValidateAccessToken::class])
+    ->prefix(Nova::path())
+    ->group(function () {
+        Route::get('/arsip-dokumen/{token}', [ArsipController::class, 'perKro'])
+            ->name('arsip-per-kro');
+        Route::get('/arsip-dokumen/{token}/kro/{kro}', [ArsipController::class, 'perDetail'])
+            ->name('arsip-per-detail');
+        Route::get('/arsip-dokumen/{token}/coa/{coa}', [ArsipController::class, 'perKak'])
+            ->name('arsip-per-kak');
+        Route::get('/arsip-dokumen/{token}/kak/{kak}', [ArsipController::class, 'daftarFile'])
+            ->name('daftar-file');
+    });

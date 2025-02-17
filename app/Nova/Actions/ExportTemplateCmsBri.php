04990926a46c45254683271e6cdce91c36eb590e
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-02-18 06:11:16 +0800

    Update CSV enclosure character in ExportTemplateCmsBri action for improved compatibility

--- app/Nova/Actions/ExportTemplateCmsBri.php
+++ app/Nova/Actions/ExportTemplateCmsBri.php
@@ -169,7 +169,7 @@ class ExportTemplateCmsBri extends Action
             $collection = $collection->merge($summary);
         }
         $filename = $fields->filename.'.csv';
-        (new FastExcel($collection))->configureCsv(delimiter: '|', enclosure: chr(0))->export(Storage::disk('temp')->path($filename));
+        (new FastExcel($collection))->configureCsv(delimiter: '|', enclosure: chr(32))->export(Storage::disk('temp')->path($filename));
 
         return Action::redirect(route('dump-download', [
             'filename' => $filename,

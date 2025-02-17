707712af68550db47e1f5e47451f6e20bf8f0d1f
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-02-17 22:34:53 +0800

    Update pagination limit in daftarFile method to enhance performance

--- app/Http/Controllers/ArsipController.php
+++ app/Http/Controllers/ArsipController.php
@@ -64,7 +64,7 @@ class ArsipController extends Controller
         $tahun = ShareLink::where('token', $token)->first()->tahun;
         $path = $tahun.'/'.'arsip-dokumens'.'/'.$kak;
         $files = Storage::disk('arsip')->files($path);
-        $perPage = 15;
+        $perPage = 2;
         $page = request()->get('page', 1);
         $offset = ($page - 1) * $perPage;
         $data = array_slice($files, $offset, $perPage);

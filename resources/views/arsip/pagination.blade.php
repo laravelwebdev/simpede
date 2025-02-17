c1982d85c42b86bd1d2ce996a436c62fc3b3b1aa
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-02-17 03:35:43 +0000

    Update pagination link class for current page to improve accessibility

--- resources/views/arsip/pagination.blade.php
+++ resources/views/arsip/pagination.blade.php
@@ -22,7 +22,7 @@
     @if (is_array($element))
     @foreach ($element as $page => $url)
     @if ($page == $paginator->currentPage())
-    <li><a class="pagination-link is-primary" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
+    <li><a class="pagination-link is-current" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
     @else
     <li><a href="{{ $url }}" class="pagination-link" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
     @endif

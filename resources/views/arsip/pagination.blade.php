12589c8e1f65aff8664bd8ae8870182e4d5041da
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-02-17 03:28:17 +0000

    Update pagination styling: change class to 'is-small' and update current page link class to 'is-primary'

--- resources/views/arsip/pagination.blade.php
+++ resources/views/arsip/pagination.blade.php
@@ -1,5 +1,6 @@
 @if ($paginator->hasPages())
-<nav class="pagination is-medium" role="navigation" aria-label="pagination">
+<b
+<nav class="pagination is-small" role="navigation" aria-label="pagination">
   @if ($paginator->onFirstPage())
   <a class="pagination-previous is-disabled" title="This is the first page">Previous</a>
   @else
@@ -21,7 +22,7 @@
     @if (is_array($element))
     @foreach ($element as $page => $url)
     @if ($page == $paginator->currentPage())
-    <li><a class="pagination-link is-current" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
+    <li><a class="pagination-link is-primary" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
     @else
     <li><a href="{{ $url }}" class="pagination-link" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
     @endif

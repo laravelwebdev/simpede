f2d3bf1d288f412b86e0213437fac847b7693806
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-02-18 10:40:31 +0000

    Refactor header layout in welcome.blade.php for improved structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,8 +27,8 @@
           />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#18B69C] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-center lg:col-start-2">
+                    <header class="grid grid-cols-2 items-center gap-2 py-10">
+                        <div class="flex">
                             <svg
                             width="360px"
                             viewBox="0 0 210 39"

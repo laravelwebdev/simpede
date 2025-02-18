af456a858cb8f2fa71bff4aa1300cab93a54019a
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-02-18 03:02:26 +0000

    Update version to 2.6.3 and implement theme switching in layout template

--- resources/views/arsip/layout.blade.php
+++ resources/views/arsip/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,23 +9,6 @@
     <link rel="stylesheet" href="{{ asset('css/main.min.css') }}">
     <script type="text/javascript" src="{{ asset('js/toast.min.js') }}"></script>
     <style type="text/css">
-      h1.title, h3.subtitle {
-      text-align: center;
-      }
-      h1.title {
-      font-weight: 900;
-      }
-      .is-main h1.title {
-      font-size: 3rem;
-      }
-      .is-main h3.subtitle {
-      font-size: 2rem;
-      }
-/**
-* Custom Simple Toast Stylesheet
-* Developed by: oretnom23
-* Please load this file with the CustomToast.js file and Google Icons
-*/
 .toast {
     position: fixed;
     top: 25px;
@@ -165,13 +148,11 @@
     </style>
   </head>
   <body>
-    <section class="hero is-light">
-      <div class="hero-body">
-        <div class="container">
+    <section class="section">
+        <div class="container has-text-centered">
           <h1 class="title">Arsip Dokumen Tahun Anggaran {{ $tahun }}</h1>
           <h3 class="subtitle">Per {{ $level }}</h3>
         </div>
-      </div>
     </section>
     <section class="section">
       <div class="container">
@@ -184,5 +165,13 @@
       </div>
     </section>
     @yield('script')
+    <script>
+      const theme =
+        localStorage.getItem("novaTheme") ||
+        (window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light");
+      document.documentElement.setAttribute("data-theme", theme);
+  </script>
   </body>
 </html>

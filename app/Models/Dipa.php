62a2ac9f578a7c8d088f797a972678374bf4bf07
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-02-19 21:47:51 +0800

    Refactor Dipa model to optimize import statements for improved clarity

--- app/Models/Dipa.php
+++ app/Models/Dipa.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 use Mostafaznv\LaraCache\CacheEntity;
 use Mostafaznv\LaraCache\Traits\LaraCache;
-use Illuminate\Support\Facades\DB;
 
 class Dipa extends Model
 {

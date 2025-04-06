6733ee9a9b326ef465f169e87dfa9876f51c57bb
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-04-06 12:21:59 +0800

    fix: Update symbolic link path for storage in filesystems configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -150,7 +150,7 @@ return [
     */
 
     'links' => [
-        base_path('public_html/storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];

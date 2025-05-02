4d4381172fcc336295f986084e32b37a60835cc3
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-05-02 07:40:45 +0000

    fix: correct maximum limit for nilai_beban_abs calculation

--- app/Models/DaftarPenilaianReward.php
+++ app/Models/DaftarPenilaianReward.php
@@ -29,7 +29,7 @@ class DaftarPenilaianReward extends Model
             $nilai_disiplin = 0.2 * (100 - (100 * $penilaian->tk + 50 * ($penilaian->tl4 + $penilaian->psw4) + 30 * ($penilaian->tl3 + $penilaian->psw3) + 20 * ($penilaian->tl2 + $penilaian->psw2) + 10 * ($penilaian->tl1 + $penilaian->psw1)));
             $nilai_disiplin_abs = $nilai_disiplin > 0 ? $nilai_disiplin : 0;
             $nilai_beban = 0.2 * 4 * $penilaian->jumlah_butir;
-            $nilai_beban_abs = $nilai_beban <= 100 ? $nilai_beban : 100;
+            $nilai_beban_abs = $nilai_beban <= 20 ? $nilai_beban : 20;
             $penilaian->nilai_kinerja = $nilai_kinerja;
             $penilaian->nilai_disiplin = $nilai_disiplin_abs;
             $penilaian->nilai_beban = $nilai_beban_abs;

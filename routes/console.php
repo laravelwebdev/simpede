2d7b31d32d2dc0caee164c115d6e71187f81fa6b
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-06-04 21:22:32 +0800

    fix: update daily update command time to 1:00 and remove runInBackground from scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -4,18 +4,15 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('reminder:send')->hourly()
     ->withoutOverlapping()
-    ->runInBackground()
     ->sentryMonitor();
 Schedule::command('action-events:clear')->daily()
     ->withoutOverlapping()
-    ->runInBackground()
     ->sentryMonitor();
 Schedule::command('holidays:sync')->daily()
     ->withoutOverlapping()
-    ->runInBackground()
     ->sentryMonitor();
 if (config('app.auto_update')) {
-    Schedule::command('simpede:update')->dailyAt('0:30')
+    Schedule::command('simpede:update')->dailyAt('1:00')
         ->withoutOverlapping()
         ->runInBackground()
         ->timezone(config('app.schedule_timezone'))

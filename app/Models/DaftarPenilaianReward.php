cec22cb1c9d79e8a1a9f97895fa1a0d3fe0f3d01
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-07-08 23:42:55 +0000

    fix: adjust performance calculation weights in DaftarPenilaianReward model

--- app/Models/DaftarPenilaianReward.php
+++ app/Models/DaftarPenilaianReward.php
@@ -25,10 +25,10 @@ class DaftarPenilaianReward extends Model
     protected static function booted(): void
     {
         static::saving(function (DaftarPenilaianReward $penilaian) {
-            $nilai_kinerja = 0.5 * $penilaian->nilai_skp;
+            $nilai_kinerja = 0.45 * $penilaian->nilai_skp;
             $nilai_kehadiran = (100 / $penilaian->hk) * ($penilaian->hd + 0.5 * $penilaian->cst + 0.5 * $penilaian->tb);
             $nilai_disiplin = 0.1 * ($nilai_kehadiran - (100 * $penilaian->tk + 10 * ($penilaian->tl4 + $penilaian->psw4) + 7.5 * ($penilaian->tl3 + $penilaian->psw3) + 5 * ($penilaian->tl2 + $penilaian->psw2) + 2.5 * ($penilaian->tl1 + $penilaian->psw1)));
-            $nilai_perilaku = 0.4 * $penilaian->nilai_perilaku;
+            $nilai_perilaku = 0.45 * $penilaian->nilai_perilaku;
             $penilaian->nilai_kinerja = $nilai_kinerja;
             $penilaian->nilai_disiplin = $nilai_disiplin;
             $penilaian->nilai_perilaku = $nilai_perilaku;

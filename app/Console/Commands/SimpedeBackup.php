0d64116ffd39e7ca39d09b372ff4f0648382bc4d
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-08-10 11:48:26 +0000

    Update backup configuration to disable retention periods and adjust cleanup thresholds

---
+++ app/Console/Commands/SimpedeBackup.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SimpedeBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'simpede:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Backup Application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('action-events:clear');
+        $this->call('queue:clear');
+        $this->call('backup:run');
+        $this->call('backup:clean');
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('reminder:send')->hourly()
     ->sentryMonitor();
-Schedule::command('action-events:clear')->daily()
-    ->sentryMonitor();
-Schedule::command('queue:clear')->daily()
-    ->sentryMonitor();
 Schedule::command('holidays:sync')->daily()
     ->sentryMonitor();
 if (config('app.auto_update')) {
@@ -20,11 +16,7 @@ Schedule::command('db:optimize')->monthlyOn(1, '02:00')
     ->withoutOverlapping()
     ->timezone(config('app.schedule_timezone'))
     ->sentryMonitor();
-Schedule::command('backup:clean')->dailyAt('16:30')
-    ->withoutOverlapping()
-    ->timezone(config('app.schedule_timezone'))
-    ->sentryMonitor();
-Schedule::command('backup:run')->dailyAt('17:00')
+Schedule::command('simpede:backup')->dailyAt('17:30')
     ->withoutOverlapping()
     ->timezone(config('app.schedule_timezone'))
     ->sentryMonitor();

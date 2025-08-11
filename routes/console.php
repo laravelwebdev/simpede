861e8d875660a743a785fc99ec6b02f09fa628e9
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2025-08-11 10:15:22 +0000

    Update scheduled commands to run in the background and adjust backup time

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,7 @@ Schedule::command('holidays:sync')->daily()
 if (config('app.auto_update')) {
     Schedule::command('simpede:update')->dailyAt('01:00')
         ->withoutOverlapping()
+        ->runInBackground()
         ->timezone(config('app.schedule_timezone'))
         ->sentryMonitor();
 }
@@ -16,7 +17,8 @@ Schedule::command('db:optimize')->monthlyOn(1, '02:00')
     ->withoutOverlapping()
     ->timezone(config('app.schedule_timezone'))
     ->sentryMonitor();
-Schedule::command('simpede:backup')->dailyAt('17:30')
+Schedule::command('simpede:backup')->dailyAt('18:30')
     ->withoutOverlapping()
+    ->runInBackground()
     ->timezone(config('app.schedule_timezone'))
     ->sentryMonitor();

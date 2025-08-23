ad9c8a334222833ec8daccebc38701423623d7a9
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-08-23 18:15:21 +0800

    Adjust image opacity in welcome.blade.php for improved visibility

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
                 width: 100%;
                 height: 100%;
                 object-fit: cover;
-                opacity: 0.9;
+                opacity: 0.75;
             }
 
              /* Default selection color */

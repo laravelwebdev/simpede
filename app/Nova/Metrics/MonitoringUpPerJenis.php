2579330e82edc5a5cb43398d6557cb3fd117adf1
Muhlis Abdi <enterpriseabdi@gmail.com>
2025-08-31 09:07:44 +0800

    Refactor MonitoringUpPerJenis to incorporate tanggal_nihil for GUP and TUP calculations

--- app/Nova/Metrics/MonitoringUpPerJenis.php
+++ app/Nova/Metrics/MonitoringUpPerJenis.php
@@ -3,6 +3,7 @@
 namespace App\Nova\Metrics;
 
 use App\Helpers\Helper;
+use App\Models\Dipa;
 use Laravel\Nova\Nova;
 use Laravelwebdev\Table\Table;
 use Laravelwebdev\Table\Table\Cell;
@@ -12,6 +13,11 @@ class MonitoringUpPerJenis extends Table
 {
     public function __construct()
     {
+        $tglNihil = optional(
+            Dipa::cache()->get('all')
+                ->where('tahun', session('year'))
+                ->first()
+        )->tanggal_nihil;
         $gup = null;
         $minGup = null;
         $tup = null;
@@ -50,12 +56,20 @@ class MonitoringUpPerJenis extends Table
         $this->data([
             Row::make(
                 Cell::make('GUP'),
-                Cell::make($gup ? Helper::terbilangTanggal($gup['akhir']) : '-'),
+                Cell::make(
+                    ($gup && $tglNihil && $gup['akhir'] > $tglNihil)
+                        ? Helper::terbilangTanggal($tglNihil)
+                        : ($gup ? Helper::terbilangTanggal($gup['akhir']) : '-')
+                ),
                 Cell::make($minGup ? Helper::formatUang($minGup) : '-')->class('text-right')
             ),
             Row::make(
                 Cell::make('TUP'),
-                Cell::make(($tup && $tup['akhir'] !== '-') ? Helper::terbilangTanggal($tup['akhir']) : ($tup['akhir'] ?? '-')),
+                Cell::make(
+                    ($tup && $tglNihil && $tup['akhir'] > $tglNihil)
+                        ? Helper::terbilangTanggal($tglNihil)
+                        : ($tup && $tup['akhir'] !== '-' ? Helper::terbilangTanggal($tup['akhir']) : ($tup['akhir'] ?? '-'))
+                ),
                 Cell::make($latestTup ? Helper::formatUang(optional($latestTup)->nilai) : '-')->class('text-right')
             ),
         ]

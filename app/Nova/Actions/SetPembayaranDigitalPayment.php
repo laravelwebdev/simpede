2d38ea51e5cdba9e37ea9847061c65fe4660a6d1
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2026-01-16 08:47:00 +0800

    fix action event

--- app/Nova/Actions/SetPembayaranDigitalPayment.php
+++ app/Nova/Actions/SetPembayaranDigitalPayment.php
@@ -17,6 +17,8 @@ class SetPembayaranDigitalPayment extends Action
     use InteractsWithQueue;
     use Queueable;
 
+    public $withoutActionEvents = true;
+
     public $name = 'Set Pembayaran Digital';
 
     /**
